7f0f88becfe5d69c325bcb33195c2eb646d06f80
BadAtRainbow6 <118947416+BadAtRainbow6@users.noreply.github.com>
2024-10-22 11:34:34 -0600

    RayTracer Project

---
+++ RayTrace/Renderer.cpp
@@ -0,0 +1,45 @@
+#include "Renderer.h"
+#include "Framebuffer.h"
+
+bool Renderer::Initialize()
+{
+    // initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool Renderer::CreateWindow(int width, int height, const char *name)
+{
+    // create window
+    // returns pointer to window if successful or nullptr if failed
+    m_window = SDL_CreateWindow(name,
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        width, height,
+        SDL_WINDOW_SHOWN);
+    if (m_window == nullptr)
+    {
+        SDL_Quit();
+        return false;
+    }
+
+    // create renderer
+    m_renderer = SDL_CreateRenderer(m_window, -1, 0);
+
+    m_width = width;
+    m_height = height;
+
+    return true;
+}
+
+void Renderer::CopyFramebuffer(const Framebuffer& framebuffer)
+{
+    SDL_RenderCopy(m_renderer, framebuffer.m_texture, NULL, NULL);
+}
+
+void Renderer::operator=(const Framebuffer& framebuffer)
+{
+    SDL_RenderCopy(m_renderer, framebuffer.m_texture, NULL, NULL);
+}
---
+++ RayTrace/Renderer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SDL.h>
+
+class Renderer 
+{
+public:
+	bool Initialize();
+	bool CreateWindow(int width, int height, const char *name);
+	void CopyFramebuffer(const class Framebuffer& framebuffer);
+
+	void operator = (const class Framebuffer& framebuffer);
+
+public:
+	SDL_Renderer* m_renderer;
+	SDL_Window* m_window;
+
+	int m_width;
+	int m_height;
+};
